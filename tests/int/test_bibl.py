8c2a1ef14e6a09dcea4ab160963ae64af6795ac3
Helmut Konrad Schewe <helmutus@outlook.com>
2023-03-09 12:47:46 +0000

    test(bibl): verify that resource does contain some content

--- tests/int/test_bibl.py
+++ tests/int/test_bibl.py
@@ -2,8 +2,15 @@ import tests
 import tests.int
 import tests.resources
 
+CITATION = 'Preferred Citation'
+FULL = 'Full Citation Information'
+ABOUT = 'About this Online Entry'
+
 
 def test_bibl_1():
     resource = tests.resources.BIBL_1_ID
     result = tests.int.curl(f'/bibl/{resource}')
     assert tests.contains_hx('Bibliography Record', result), result
+    assert ABOUT in result
+    assert CITATION in result
+    assert FULL in result
