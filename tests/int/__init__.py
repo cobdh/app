930b00089a60cb3c3b85ecb4db503c09c702b8d9
Helmut Konrad Schewe <helmutus@outlook.com>
2023-03-31 15:00:24 +0000

    test(int): reduce white spaces in html to ease testing

--- tests/int/__init__.py
+++ tests/int/__init__.py
@@ -1,4 +1,5 @@
 import os
+import re
 import urllib.request
 
 import werkzeug.urls
@@ -9,7 +10,7 @@ SERVER = os.environ['SERVER_TEST'].rstrip('/')
 REST = os.environ['SERVER_REST'].rstrip('/')
 
 
-def curl(path: str):
+def curl(path: str, optimize: bool = True):
     """\
     >>> curl('/')
     '...<h1 data-template="config:app-title">cobdh.org data</h1>...'
@@ -19,6 +20,11 @@ def curl(path: str):
     with urllib.request.urlopen(url) as response:  # nosec
         html = response.read()
     html: str = html.decode('utf8')
+    if optimize:
+        # reduce useless white spaces to ease testing
+        html = html.replace('\n', '')
+        # reduce multiple white spaces
+        html = re.sub(r'[ ]+', ' ', html)
     return html
 
 
