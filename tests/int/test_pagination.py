d99a15aff934ed14100c77f3514f6291497eac75
Helmut Konrad Schewe <helmutus@outlook.com>
2023-04-07 15:07:27 +0000

    test(bibl): verify pagination

--- tests/int/test_pagination.py
+++ tests/int/test_pagination.py
@@ -1,16 +1,28 @@
+import pytest
+
 import tests.int
 
 
-def test_persons_paging():
+@pytest.mark.parametrize('collection', (
+    'bibl',
+    'persons',
+))
+def test_paging(collection):
+    """Ensure that forward and backward button, e.g. pagination is rendered."""
     # use small number to show pagination
-    result = tests.int.curl(f'/persons?perpage=3')
+    result = tests.int.curl(f'/{collection}?perpage=3')
     assert 'Next' in result
     assert 'Prev' not in result
-    result = tests.int.curl(f'/persons?start=3&perpage=3')
+    result = tests.int.curl(f'/{collection}?start=3&perpage=3')
     assert 'Prev' in result
 
 
-def test_persons_nopaging():
+@pytest.mark.parametrize('collection', (
+    'bibl',
+    'persons',
+))
+def test_nopaging(collection):
+    """All elements are on the same page."""
     # use large number to hide
-    result = tests.int.curl(f'/persons?perpage=30000')
+    result = tests.int.curl(f'/{collection}?perpage=30000')
     assert 'Next' not in result
