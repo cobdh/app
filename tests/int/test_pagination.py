8401fa3b10d53056bffd63b372c13fb0b7d17221
Helmut Konrad Schewe <helmutus@outlook.com>
2023-04-07 17:26:43 +0000

    test(pagination): verify search bibl pagination

--- tests/int/test_pagination.py
+++ tests/int/test_pagination.py
@@ -6,20 +6,23 @@ import tests.int
 @pytest.mark.parametrize('collection', (
     'bibl',
     'persons',
+    'search?bibl_keyword=a',
 ))
 def test_paging(collection):
     """Ensure that forward and backward button, e.g. pagination is rendered."""
     # use small number to show pagination
-    result = tests.int.curl(f'/{collection}?perpage=3')
+    quote = '?' if '?' not in collection else '&'
+    result = tests.int.curl(f'/{collection}{quote}perpage=3')
     assert 'Next' in result
     assert 'Prev' not in result
-    result = tests.int.curl(f'/{collection}?start=3&perpage=3')
+    result = tests.int.curl(f'/{collection}{quote}start=3&perpage=3')
     assert 'Prev' in result
 
 
 @pytest.mark.parametrize('collection', (
     'bibl',
     'persons',
+    'search?bibl_keyword=a',
 ))
 def test_nopaging(collection):
     """All elements are on the same page."""
