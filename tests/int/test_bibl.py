a6216b242cc41d0f3f02d2c7ef9d766f45bfecfa
Helmut Konrad Schewe <helmutus@outlook.com>
2023-04-25 08:05:51 +0000

    test(bibl): ensure that bibl is sorted by date

--- tests/int/test_bibl.py
+++ tests/int/test_bibl.py
@@ -1,3 +1,5 @@
+import re
+
 import tests
 import tests.int
 import tests.resources.ids
@@ -35,3 +37,11 @@ def test_export_tei_bibl():
     assert '<TEI xmlns="http://www.tei-c.org/ns/1.0" xml:lang="en">' in result
     assert '<biblFull xml:id="BVCP1990">' in result
     assert '</body></TEI>' in result
+
+
+def test_bibl_sortedby_year():
+    """Ensure that bibl is sorted by date."""
+    result = tests.int.curl('/bibl')
+    numbers = re.findall(r'(\d{4}):', result)
+    numbers = [int(item) for item in numbers]
+    assert numbers == sorted(numbers, reverse=True)
