6ae0fe9cb7864aae983ca4c7f159ea633fdb9a70
Helmut Konrad Schewe <helmutus@outlook.com>
2023-05-31 10:27:25 +0000

    test(editor): verify new editor view

--- tests/int/test_editor.py
+++ tests/int/test_editor.py
@@ -8,6 +8,10 @@ def test_editors_schewe_helmut_kornad():
     assert 'Schewe' in result, result
     assert tests.contains_hx('Edited', result, 3), result
     assert tests.contains_hx('XML coded', result, 3), result
+    # persons and bibl
+    assert result.count('XML coded') == 2
+    # persons and bibl
+    assert result.count('Edited') == 2
 
 
 def test_editors_phenix():
