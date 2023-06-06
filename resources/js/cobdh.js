e944b9432fdaef33cb16492d5d693c459e23c6f5
Helmut Konrad Schewe <helmutus@outlook.com>
2023-06-06 16:02:14 +0000

    chore(clipboard): use external clipboard
    + upgrade version

--- resources/js/cobdh.js
+++ resources/js/cobdh.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
     //Clipboard function for any buttons with clipboard class. Uses clipboard.js
-    var clipboard = new Clipboard('.clipboard');
+    var clipboard = new ClipboardJS('.clipboard');
 });
